31f7aa04c4c572fbdad01c1924af5b5d1ab56eef
John D. Jameson <thejohnjameson@gmail.com>
2014-09-07 15:07:47 -0400

    Add Save Progress spec

---
+++ spec/javascripts/saveProgress.spec.js
@@ -0,0 +1,28 @@
+describe('Spellbook.saveProgress', function() {
+  beforeEach(function() {
+    loadFixtures('save_progress.html');
+    this.element = $('.js-saveProgress');
+    this.container = $('.js-saveProgress-container');
+    this.key = this.element.data('saveprogress');
+    return this.value = 'string';
+  });
+  it('should save input element values to localStorage on input event', function() {
+    Spellbook.saveProgress.init();
+    this.element.val(this.value);
+    this.element.trigger('input');
+    return expect(localStorage.getItem(this.key)).toEqual(this.element.val());
+  });
+  it('should fill input elements with localStorage values when initialized', function() {
+    this.element.val('');
+    Spellbook.saveProgress.init();
+    return expect(this.element.val()).toEqual(this.value);
+  });
+  return it('should remove container-specific localStorage items on form submission', function() {
+    Spellbook.saveProgress.init();
+    localStorage.setItem(this.key, this.value);
+    localStorage.setItem('keep', 'me');
+    this.container.trigger('submit');
+    expect(localStorage.getItem(this.key)).toEqual(null);
+    return expect(localStorage.getItem('keep')).toEqual('me');
+  });
+});
---
+++ spec/saveProgress.spec.coffee
@@ -0,0 +1,33 @@
+describe 'Spellbook.saveProgress', ->
+  beforeEach ->
+    loadFixtures('save_progress.html')
+
+    @element   = $('.js-saveProgress')
+    @container = $('.js-saveProgress-container')
+    @key       = @element.data('saveprogress')
+    @value     = 'string'
+
+  it 'should save input element values to localStorage on input event', ->
+    Spellbook.saveProgress.init()
+
+    @element.val(@value)
+    @element.trigger('input')
+
+    expect( localStorage.getItem(@key) ).toEqual( @element.val() )
+
+  it 'should fill input elements with localStorage values when initialized', ->
+    @element.val('')
+
+    Spellbook.saveProgress.init()
+
+    expect( @element.val() ).toEqual(@value)
+
+  it 'should remove container-specific localStorage items on form submission', ->
+    Spellbook.saveProgress.init()
+
+    localStorage.setItem( @key, @value )
+    localStorage.setItem( 'keep', 'me' )
+    @container.trigger('submit')
+
+    expect( localStorage.getItem(@key) ).toEqual(null)
+    expect( localStorage.getItem('keep') ).toEqual('me')
