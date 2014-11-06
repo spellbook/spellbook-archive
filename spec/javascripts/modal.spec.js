61dbf79df43c93afb09fae154ab6d1770a0509a2
Drew Barontini <drewbarontini@gmail.com>
2014-11-06 08:14:16 -0500

    Add Spellbook.modal spec

---
+++ spec/javascripts/modal.spec.js
@@ -0,0 +1,32 @@
+describe('Spellbook.modal', function() {
+  beforeEach(function() {
+    loadFixtures('modal.html');
+    this.trigger = $('.js-modal-trigger');
+    this.close = $('.js-modal-close');
+    this.modal = $('#modal');
+    this.backdrop = $('.modal-backdrop');
+    this.activeClass = 'is-active';
+    return Spellbook.modal.init();
+  });
+  it('should trigger a click event when the link is clicked', function() {
+    spyOnEvent(this.trigger, 'click');
+    this.trigger.click();
+    return expect('click').toHaveBeenTriggeredOn(this.trigger);
+  });
+  it('should open the modal when the trigger is clicked', function() {
+    this.trigger.click();
+    return expect(this.modal).toHaveClass(this.activeClass);
+  });
+  it('should inject a backdrop element into the page', function() {
+    this.trigger.click();
+    return expect(this.backdrop).toBeInDOM();
+  });
+  it('should close the modal when the close button is clicked', function() {
+    this.close.click();
+    return expect(this.modal).not.toHaveClass(this.activeClass);
+  });
+  return it('should close the modal when the backdrop is clicked', function() {
+    this.backdrop.click();
+    return expect(this.modal).not.toHaveClass(this.activeClass);
+  });
+});
