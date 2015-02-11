5fcac7e7a65c6ffa515cc2e4981e4452a2e5f0ff
Drew Barontini <drewbarontini@gmail.com>
2015-02-11 09:51:43 -0500

    Update Spellbook.LiveSearch build

--- components/js/liveSearch.js
+++ components/js/liveSearch.js
@@ -1,5 +1,5 @@
 this.Spellbook.LiveSearch = (function() {
-  var init, _isQueryFound, _parseDom, _query, _setEventHandlers, _settings;
+  var init, _clearEmptyMessage, _handleEmptyResults, _isEmpty, _isQueryAbsent, _parseDom, _query, _setEventHandlers, _settings;
   _settings = {};
   _query = '';
   init = function(options) {
@@ -8,9 +8,12 @@ this.Spellbook.LiveSearch = (function() {
       $query: $('.js-search-query'),
       itemNode: '.js-search-item',
       hiddenClass: 'is-hidden',
-      onKeyup: null,
+      emptyMessage: true,
+      emptyNode: '.js-search-empty',
+      onClear: null,
+      onEmpty: null,
       onFound: null,
-      onEmpty: null
+      onKeyup: null
     }, options);
     return _setEventHandlers();
   };
@@ -22,30 +25,52 @@ this.Spellbook.LiveSearch = (function() {
       }
       if (_query === '') {
         $(_settings.itemNode).removeClass(_settings.hiddenClass);
+        _clearEmptyMessage();
+        if (_settings.onClear !== null) {
+          _settings.onClear(_settings);
+        }
       }
+      _clearEmptyMessage();
       return _parseDom();
     });
   };
   _parseDom = function() {
-    console.log($(_settings.itemNode).hasClass(_settings.hiddenClass));
-    return _settings.$query.each(function(index) {
+    _settings.$query.each(function(index) {
       var $element;
       $element = $(this);
-      if (_isQueryFound($element)) {
-        $element.closest(_settings.itemNode).addClass(_settings.hiddenClass);
+      if (_isQueryAbsent($element)) {
+        return $element.closest(_settings.itemNode).addClass(_settings.hiddenClass);
+      } else {
+        $element.closest(_settings.itemNode).removeClass(_settings.hiddenClass);
         if (_settings.onFound !== null) {
           return _settings.onFound(_settings);
         }
-      } else {
-        $element.closest(_settings.itemNode).removeClass(_settings.hiddenClass);
-        if (_settings.onEmpty !== null) {
-          return _settings.onEmpty(_settings);
-        }
       }
     });
+    return _handleEmptyResults();
   };
-  _isQueryFound = function(element) {
+  _clearEmptyMessage = function() {
+    if (_settings.emptyMessage && $(_settings.emptyNode).length > 0) {
+      return $(_settings.emptyNode).remove();
+    }
+  };
+  _handleEmptyResults = function() {
+    var emptyClass;
+    if (_isEmpty()) {
+      if (_settings.emptyMessage) {
+        emptyClass = _settings.emptyNode.replace('.', '');
+        $("<p class='" + emptyClass + "'>There are no results matching '" + _query + "'.</p>").insertAfter($(_settings.itemNode).last());
+      }
+      if (_settings.onEmpty !== null) {
+        return _settings.onEmpty(_settings);
+      }
+    }
+  };
+  _isQueryAbsent = function(element) {
     return element.text().search(new RegExp(_query, 'i')) < 0;
+  };
+  _isEmpty = function() {
+    return $("" + _settings.itemNode + "." + _settings.hiddenClass).length === $(_settings.itemNode).length;
   };
   return {
     init: init
