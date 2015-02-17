bf78ca3821c46bf96f50d720d0b4edd37ba04c09
John D. Jameson <thejohnjameson@gmail.com>
2015-02-17 10:41:56 -0500

    Fix Gulpfile variable assignment delimiter

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,4 @@
-var gulp       = require('gulp');
+var gulp       = require('gulp'),
     coffee     = require('gulp-coffee'),
     gutil      = require('gulp-util'),
     watch      = require('gulp-watch'),
