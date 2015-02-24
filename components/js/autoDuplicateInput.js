d599f3e4d25123cfc249034238aa12e9e18b8dd4
Drew Barontini <drewbarontini@gmail.com>
2015-02-24 11:09:30 -0500

    Add Spellbook.AutoDuplicateInput

---
+++ components/js/autoDuplicateInput.js
@@ -0,0 +1,73 @@
+this.Spellbook.AutoDuplicateInput = (function() {
+  var getCount, init, _count, _duplicate, _field, _getValidationType, _setEventHandlers, _setInputState, _settings, _validate, _validators;
+  _settings = {};
+  _count = 0;
+  _field = null;
+  _validators = {
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  };
+  init = function(options) {
+    _settings = $.extend({
+      $element: $('.js-autoDuplicateInput'),
+      $container: $('.js-autoDuplicateInput-container'),
+      clonedDataAttribute: 'cloned',
+      validateDataAttribute: 'validate',
+      invalidClass: 'is-invalid',
+      validClass: 'is-valid',
+      onDuplicate: null,
+      onInvalid: null,
+      onValid: null
+    }, options);
+    return _setEventHandlers();
+  };
+  _setEventHandlers = function() {
+    return _settings.$element.on('keyup', function(event) {
+      event.preventDefault();
+      _field = $(this);
+      if (_validate()) {
+        _setInputState('valid');
+        if (_settings.onValid !== null) {
+          _settings.onValid(_settings);
+        }
+        if (_field.data('cloned') !== 'true') {
+          _duplicate();
+        }
+        if (_settings.onDuplicate !== null) {
+          return _settings.onDuplicate(_settings, _count);
+        }
+      } else {
+        _setInputState('invalid');
+        if (_settings.onInvalid !== null) {
+          return _settings.onInvalid(_settings);
+        }
+      }
+    });
+  };
+  _getValidationType = function() {
+    return _field.data(_settings.validateDataAttribute);
+  };
+  _validate = function() {
+    var validator;
+    validator = _getValidationType(_field);
+    return _validators["" + validator].test(_field.val());
+  };
+  _duplicate = function() {
+    ++_count;
+    return _field.data(_settings.clonedDataAttribute, 'true').clone(true).appendTo(_settings.$container).removeClass(_settings.validClass).val('').data(_settings.clonedDataAttribute, '');
+  };
+  _setInputState = function(type) {
+    switch (type) {
+      case 'invalid':
+        return _field.removeClass(_settings.validClass).addClass(_settings.invalidClass);
+      case 'valid':
+        return _field.removeClass(_settings.invalidClass).addClass(_settings.validClass);
+    }
+  };
+  getCount = function() {
+    return _count;
+  };
+  return {
+    init: init,
+    getCount: getCount
+  };
+})();
---
+++ spec/autoDuplicateInput.spec.coffee
@@ -0,0 +1,44 @@
+describe 'Spellbook.AutoDuplicateInput', ->
+  beforeEach ->
+    loadFixtures( 'auto_duplicate_input.html' )
+
+    @selector              = '.js-autoDuplicateInput'
+    @element               = $( @selector )
+    @container             = $( '.js-autoDuplicateInput-container' )
+    @clonedDataAttribute   = 'cloned'
+    @validateDataAttribute = 'validate'
+    @invalidClass          = 'is-invalid'
+    @validClass            = 'is-valid'
+
+    @adi = Spellbook.AutoDuplicateInput
+    @adi.init()
+
+  it 'should register a keyup event on the input', ->
+    spyOnEvent( @element, 'keyup' )
+    @element.trigger( 'keyup' )
+
+    expect( 'keyup' ).toHaveBeenTriggeredOn( @element )
+
+  it 'should mark the input as invalid and give the appropriate class', ->
+    @element.val('thisisapartialemail')
+    @element.trigger( 'keyup' )
+
+    expect( @element ).toHaveClass( @invalidClass )
+
+  it 'should mark the input as valid and give the appropriate class', ->
+    @element.val('email@example.com')
+    @element.trigger( 'keyup' )
+
+    expect( @element ).toHaveClass( @validClass )
+
+  it 'should duplicate the input when a valid string is provided', ->
+    @element.val('email@example.com')
+    @element.trigger( 'keyup' )
+
+    expect( $( @selector ).length ).toEqual( 2 )
+
+  it 'should keep a count of valid fields', ->
+    @element.val('email@example.com')
+    @element.trigger( 'keyup' )
+
+    expect( @adi.getCount() ).toEqual( 3 )
