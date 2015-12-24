437e39e51d88b67fdabeccfca15da19144854fde
Drew Barontini <drewbarontini@gmail.com>
2015-12-24 08:38:54 -0500

    Add Classes.Singleton test

---
+++ test/source/singleton.coffee
@@ -0,0 +1,8 @@
+describe 'Spellbook.Classes.Singleton', ->
+
+  beforeEach ->
+    @a = Spellbook.Classes.Singleton.getInstance()
+    @b = Spellbook.Classes.Singleton.getInstance()
+
+  it 'should set both objects to the same instance', ->
+    expect( @a ).to.equal( @b )
--- test/test.js
+++ test/test.js
@@ -588,6 +588,16 @@ describe('Spellbook.Services.scrollTo', function() {
   });
 });
 
+describe('Spellbook.Classes.Singleton', function() {
+  beforeEach(function() {
+    this.a = Spellbook.Classes.Singleton.getInstance();
+    return this.b = Spellbook.Classes.Singleton.getInstance();
+  });
+  return it('should set both objects to the same instance', function() {
+    return expect(this.a).to.equal(this.b);
+  });
+});
+
 describe('Spellbook.Helpers.slugify', function() {
   beforeEach(function() {
     this.string = 'This is my title!';
